c7113ed97ec3bc1340aefaab6f8c175d1922df06
Adam Plondke <adam.plondke@gmail.com>
2018-12-18 14:20:01 -0600

    Day 18 part a working

--- Day18.py
+++ Day18.py
@@ -51,7 +51,8 @@ NOTES:
 """
 import numpy as np
 
-with open('Inputs\Day18TEST.input', 'r') as f:
+#with open('Inputs\Day18TEST.input', 'r') as f:
+with open('Inputs\Day18.input', 'r') as f:
     rawData = f.read().splitlines()
 
 Map = np.zeros([len(rawData), len(rawData[0])], 'str')
@@ -59,7 +60,7 @@ for idx, line in enumerate(rawData):
     for jdx, value in enumerate(line):
         Map[idx, jdx] = value 
 
-print Map
+#print Map
 
 
 ##################
@@ -81,7 +82,7 @@ def GetSurround(ydx,xdx):
         N = ' '
 
     # NORTHEAST
-    if ydx > 0 and xdx < xLen-2:
+    if ydx > 0 and xdx < xLen-1:
         NE = Map[(ydx-1, xdx+1)]
     else:
         NE = ' '
@@ -93,59 +94,200 @@ def GetSurround(ydx,xdx):
         W = ' '
 
     # EAST
-    if xdx < xLen-2:
+    if xdx < xLen-1:
         E  = Map[(ydx, xdx+1)]
     else:
         E = ' '
 
 
     # SOUTHWEST
-    if ydx < yLen-2 and xdx > 0:
+    if ydx < yLen-1 and xdx > 0:
         SW = Map[(ydx+1, xdx-1)]
     else:
         SW = ' '
 
     #SOUTH
-    if ydx < yLen-2:
+    if ydx < yLen-1:
         S  = Map[(ydx+1, xdx)]
     else:
         S = ' '
 
     # SOOUTHEAST
-    if ydx < yLen-2 and xdx < xLen-2:
+    if ydx < yLen-1 and xdx < xLen-1:
         SE = Map[(ydx+1, xdx+1)]
     else:
         SE = ' '
     return [NW, N, NE, W, E, SW, S, SE]
 
-print 'Testing GetSurround with Day18TEST.input'
-print 'The line below should equal [\' \', \' \', \' \', \' \', \'#\', \' \', \'.\', \'.\']'
-print GetSurround(0,0)
+#print 'Testing GetSurround with Day18TEST.input'
+#print 'The line below should equal [\' \', \' \', \' \', \' \', \'#\', \' \', \'.\', \'.\']'
+#print GetSurround(0,0)
 
 
 def CountTrees(idx, jdx):
     surround = GetSurround(idx, jdx)
     return surround.count('|')
 
-print "Testing CountTrees with Day18TEST.input"
-print "The line below should = 0"
-print CountTrees(0,0)
-print ''
-print 'The line below should = 2'
-print CountTrees(3,1)
+#print "Testing CountTrees with Day18TEST.input"
+#print "The line below should = 0"
+#print CountTrees(0,0)
+#print ''
+#print 'The line below should = 2'
+#print CountTrees(3,1)
 
 def CountLumber(idx, jdx):
     surround = GetSurround(idx, jdx)
     return surround.count('#')
 
-print "Testing CountLumber with Day18TEST.input"
-print "The line below should = 1"
-print CountLumber(0,0)
-print ''
-print 'The line below should = 2'
-print CountLumber(0,2)
-print ''
-print 'The line below should = 2'
-print CountLumber(5,1)
+#print "Testing CountLumber with Day18TEST.input"
+#print "The line below should = 1"
+#print CountLumber(0,0)
+#print ''
+#print 'The line below should = 2'
+#print CountLumber(0,2)
+#print ''
+#print 'The line below should = 2'
+#print CountLumber(5,1)
+
+def EmptyLandTest(ydx, xdx):
+    """
+    if 3 or more are |:
+        . -> |
+    else:
+        nothing
+
+    if nSurroundTrees >= 3:
+        . becomes |
+    else:
+        nothing
+        """
+    if Map[(ydx, xdx)] != '.': #oops, wrong input
+        return Map[(ydx, xdx)]
+    else:
+        nTrees = CountTrees(ydx, xdx)
+        if nTrees >= 3:
+            return '|'
+        else:
+            return '.'
+
+#print 'Testing EmptyLandTest with Day18TEST.input'
+#print "The line below should = \'.\'"
+#print EmptyLandTest(0,0)
+#print ''
+#print 'The line below should = |'
+#print EmptyLandTest(5,4)
+
+def TreeLandTest(ydx, xdx):
+    """
+    if 3 or more are #
+        | -> #
+    else:
+        nothing
+
+    if nsurroundLumber >= 3:
+        | becomes #
+    else:
+        nothing
+        """
+    if Map[(ydx, xdx)] != '|': #oops, wrong input
+        return Map[(ydx, xdx)]
+    else:
+        nLumber = CountLumber(ydx, xdx)
+        if nLumber >= 3:
+            return '#'
+        else:
+            return '|'
+
+#print 'Testing TreeLandTest with Day18TEST.input'
+#print "The line below should = \'.\'"
+#print TreeLandTest(0,1)
+#print ''
+#print 'The line below should = #'
+#print TreeLandTest(0,7)
+
+def LumberLandTest(ydx, xdx):
+    """
+    if 1 or more are # AND 1 or more |:
+        # remains
+    else:
+        # -> . 
 
 
+    if nSurroundLumber >= 1 AND nSurroundTrees >= 1:
+        nothing
+    else:
+        # becomes .
+        """
+    if Map[(ydx, xdx)] != '#': # oops, wrong input
+        return Map[(ydx, xdx)]
+    else:
+        nTrees = CountTrees(ydx,xdx)
+        nLumber = CountLumber(ydx,xdx)
+        if nTrees >= 1 and nLumber >= 1:
+            return '#' # nothing changes
+        else:
+            return '.' # changes to empty
+#print 'Testing LumberLandTest with Day18TEST.input'
+#print "The line below should = #"
+#print LumberLandTest(0,8)
+#print ''
+#print 'The line below should = .'
+#print LumberLandTest(0,1)
+
+def NextGen(ydx, xdx):
+    if Map[(ydx, xdx)] == '.':
+        char = EmptyLandTest(ydx, xdx) 
+    elif Map[(ydx, xdx)] == '|':
+        char = TreeLandTest(ydx, xdx)
+    elif Map[(ydx, xdx)] == '#':
+        char = LumberLandTest(ydx, xdx)
+    else:
+        print "Crap! Wrong Map input"
+    return char
+
+def PrintMap(inMap):
+    for i in inMap:
+        print ''.join(i)
+
+NewMap = Map.copy()
+t = 0 #time = 0 mins
+#while t < 10:
+lastGen = 0
+while t < 1000000000:
+#while t < 2:
+    t += 1
+    for ydx in range(len(Map)):
+        for xdx in range(len(Map[0])):
+            NewMap[(ydx, xdx)] = NextGen(ydx, xdx)
+    print "After ", t, "mins"
+    #PrintMap(NewMap)
+    print "\n"
+    Map = NewMap.copy()
+    nTrees = 0
+    nLumber = 0 
+    for ydx in range(len(Map)):
+        for xdx in range(len(Map[0])):
+            if Map[(ydx, xdx)] == '|':
+                nTrees += 1
+            elif Map[(ydx, xdx)] == '#':
+                nLumber += 1
+    resource = nTrees * nLumber
+    print "Delta", resource - lastGen
+    lastGen = resource
+
+"""
+nTrees = 0
+nLumber = 0 
+for ydx in range(len(Map)):
+    for xdx in range(len(Map[0])):
+        if Map[(ydx, xdx)] == '|':
+            nTrees += 1
+        elif Map[(ydx, xdx)] == '#':
+            nLumber += 1
+
+#PrintMap(Map)
+answer = nTrees * nLumber
+print "nTrees = ", nTrees
+print "nLumber = ", nLumber
+print "answer = ", answer 
+"""
